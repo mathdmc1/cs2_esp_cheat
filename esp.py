cbad552d1e1a0e664b23efdd9347341332ad36b8
mathdmc1 <mathiarrua@gmail.com>
2026-01-04 23:07:46 -0300

    esp

---
+++ esp.py
@@ -0,0 +1,86 @@
+import pymem,pymem.process,keyboard
+import dumper
+from win32api import GetSystemMetrics
+import struct
+import overlay
+client = dumper.Client()
+dwEntityList = client.offset('dwEntityList')
+dwLocalPlayerPawn = client.offset('dwLocalPlayerPawn')
+m_iIDEntIndex = client.client_dll('C_CSPlayerPawn', 'm_iIDEntIndex')
+m_iTeamNum = client.client_dll('C_BaseEntity', 'm_iTeamNum')
+m_iHealth = client.client_dll('C_BaseEntity', 'm_iHealth')
+m_vOldOrigin=client.client_dll('C_BasePlayerPawn', 'm_vOldOrigin')
+dwViewMatrix = client.offset('dwViewMatrix')
+pm = pymem.Pymem("cs2.exe")
+client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+
+check_team=True
+
+def main():
+    list_entity_mapped=[]
+    dibujar = overlay.ScreenDrawer()
+    while True:
+        dibujar.clear()
+        try:
+            player = pm.read_longlong(client + dwLocalPlayerPawn)
+            playerTeam = pm.read_int(player + m_iTeamNum)
+            entityId = pm.read_int(player + m_iIDEntIndex)
+            enemys_pos=get_enemys_pos(list_entity_mapped)
+            
+            view_matrix = pm.read_bytes(client + dwViewMatrix, 64)
+            matrix = struct.unpack("16f", view_matrix)
+            if enemys_pos!=None:
+                for i in enemys_pos:
+                        screen_cords = world_to_screen(matrix, (i[0], i[1], i[2]))
+                        if screen_cords:
+                            dibujar.draw_canvas(int(screen_cords[0]), int(screen_cords[1]),int(screen_cords[2]))
+                            
+                            
+            entList = pm.read_longlong(client + dwEntityList)
+            entEntry = pm.read_longlong(entList + 0x8 * (entityId >> 9) + 0x10)
+            entity = pm.read_longlong(entEntry + 112 * (entityId & 0x1FF))
+            entityTeam = pm.read_int(entity + m_iTeamNum)
+            
+            if (entity not in list_entity_mapped) and (entityTeam == 2 or entityTeam == 3) and (entityTeam!=playerTeam or check_team==False):
+                list_entity_mapped.append(entity)
+        except:
+            pass
+        
+        dibujar.root.update()
+        #dibujar.root.mainloop()
+
+def get_enemys_pos(list_entity_mapped):
+    if len(list_entity_mapped) == 0:
+        return None
+    enemys_pos=[]
+    for i in list_entity_mapped:
+        if pm.read_int(i + m_iHealth) <= 0 and pm.read_int(i + m_iTeamNum) not in [2, 3]:
+            continue
+        pos=read_vec3(pm, i + m_vOldOrigin)
+        enemys_pos.append(pos)
+
+    return enemys_pos
+
+
+def read_vec3(pm, addr):
+    return (
+        pm.read_float(addr),
+        pm.read_float(addr + 4),
+        pm.read_float(addr + 8)
+    )
+
+def world_to_screen(matrix, pos):
+    z = pos[0] * matrix[12] + pos[1] * matrix[13] + (pos[2]) * matrix[14] + matrix[15]
+    # person behind us
+    if z < 0.01:
+        return None
+    x = pos[0] * matrix[0] + pos[1] * matrix[1] + pos[2] * matrix[2] + matrix[3]
+    y = pos[0] * matrix[4] + pos[1] * matrix[5] + pos[2] * matrix[6] + matrix[7]
+    xx = x / z
+    yy = y / z
+    _x = (GetSystemMetrics(0) / 2 * xx) + (xx + GetSystemMetrics(0) / 2)
+    _y = (GetSystemMetrics(1) / 2) - (GetSystemMetrics(1) / 2 * yy)
+    return _x, _y, z
+
+main()
---
+++ overlay.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+import win32api,win32con,time,random
+from win32api import GetSystemMetrics
+class ScreenDrawer:
+    def __init__(self):
+        self.root = tk.Tk()
+
+        self.width = self.root.winfo_screenwidth()
+        self.height = self.root.winfo_screenheight()
+
+        self.root.overrideredirect(True)
+        self.root.attributes("-topmost", True)
+        self.root.geometry(f"{self.width}x{self.height}+0+0")
+
+        self.transparent_color = "magenta"
+        self.root.configure(bg=self.transparent_color)
+        self.root.attributes("-transparentcolor", self.transparent_color)
+        self.root.bind("<Button-1>", self.on_canvas_click)
+        self.canvas = tk.Canvas(
+            self.root,
+            bg=self.transparent_color,
+            highlightthickness=0,takefocus=False
+        )
+        self.canvas.pack(fill="both", expand=True)
+    
+
+    def on_canvas_click(self, event):
+            # Check if the click occurred within the region occupied by the drawn elements
+            x, y = event.x, event.y
+            if self.canvas.find_overlapping(x, y, x, y):
+                # release if clicked on overlay so it doesnt hang
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                # Simulate a click at the calculated position to get focus back
+                win32api.SetCursorPos((x - 50, y - 2000))
+                time.sleep(0.08)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                time.sleep(random.uniform(0.01, 0.03))
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+            return "break"
+    def clear(self):
+        self.canvas.delete("all")
+
+    def on_canvas_click(self, event):
+            # Check if the click occurred within the region occupied by the drawn elements
+        x, y = event.x, event.y
+        if self.canvas.find_overlapping(x, y, x, y):
+                # release if clicked on overlay so it doesnt hang
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                # Simulate a click at the calculated position to get focus back
+            win32api.SetCursorPos((x - 50, y - 2000))
+            time.sleep(0.08)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+            time.sleep(random.uniform(0.01, 0.03))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        return "break"
+
+    def draw_circle(self, x, y, r=15, color="red"):
+        return self.canvas.create_oval(x - r, y - r, x + r, y + r, outline=color, width=2)
+    
+
+    def draw_canvas(self, x, y,z):
+        
+            # Adjust rectangle size based on Z-distance and screen size
+            rect_size = 100000 / z
+            rect_width = rect_size / 3.3
+            rect_height = rect_size / 1.9
+
+            # Calculate rectangle coordinates
+            rect_x1 = x - rect_width / 2
+            rect_y1 = y
+            rect_x2 = x + rect_width / 2
+            rect_y2 = y + rect_height * -1
+
+            # Draw rectangle with outline
+            self.canvas.create_rectangle(rect_x1, rect_y1, rect_x2, rect_y2, outline="#00ff00")
+            # Draw line from bottom center to crosshair middle
+            bottom_center_x = GetSystemMetrics(0) // 2
+            bottom_center_y = GetSystemMetrics(1)
+            self.canvas.create_line(bottom_center_x, bottom_center_y, x, y, fill="#0000bb")
+  # Replace with the actual health value for each enemy
+            
+    def run(self):
+        self.root.mainloop()
